3f697b44df3703e06be26314492a25f2d57b6d16
Serge Toarca <serge@toarca.com>
2024-12-12 07:38:05 -0500

    Add install for pftc

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+set -e
+
+# Symlink project_files_to_clipboard.sh to /usr/bin as 'pftc'
+sudo ln -sf "$(pwd)/project_files_to_clipboard.sh" /usr/bin/pftc
+
--- project_files_to_clipboard.sh
+++ project_files_to_clipboard.sh
@@ -17,7 +17,7 @@ else
   fi
 fi
 
-OUTPUT="I've attached all the relevant files. Please print out updated versions of all files. Each file should be in its own code block. Please make only the changes I've asked and no others. In particular, do not modify or delete any comments unless they're directly related to your changes. Do not add any comments unless they would be useful to someone reading this code in a year.\n\n"
+OUTPUT="I've attached all the relevant files. Please print out updated versions of all files that have changed. Do not print out unchanged files. Each file should be in its own code block. Please make only the changes I've asked and no others. In particular, do not modify or delete any comments unless they're directly related to your changes. Do not add any comments unless they would be useful to someone reading this code in a year.\n\n"
 
 for f in $FILES; do
   OUTPUT+="// FILE: $(realpath $f)\n"
